1659f70f2423604156556469bd9ce33704da9484
Lore0905 <lore.castelli4@gmail.com>
2022-02-16 15:16:42 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <nav class="navbar navbar-dark bg-primary">
+        <div class="container">
+            <ul>
+                <li>
+                    <a href="#">Comics List</a>
+                </li>
+                <li>
+                    <a href="#">Details</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layout/app')
+
+@section('title-page')
+Homepage
+@endsection
+
+@section('main-content')
+    @include('main.main')
+@endsection
